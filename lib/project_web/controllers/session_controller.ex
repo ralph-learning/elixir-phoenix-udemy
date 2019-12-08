2a032ec3fada346b4329aa93a548cb8255132561
Ralph Effting <ralfting@gmail.com>
2019-12-08 13:59:02 -0300

    Create strategy for authentication Handle authentication cases (:ok and :error)

---
+++ lib/project_web/controllers/session_controller.ex
@@ -0,0 +1,25 @@
+defmodule Project.SessionController do
+  use Project, :controller
+
+  alias Project.Accounts
+
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"user" => %{"email" => email, "password" => password}}) do
+    case Accounts.authenticate_by_email_password(email, password) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> configure_session(renew: true)
+        |> redirect(to: '/')
+
+      {:error, :unauthorized} ->
+        conn
+        |> put_flash(:error, "Bad email/password")
+        |> reidrected(to: session_path(conn, :new))
+    end
+  end
+end
--- lib/project_web/router.ex
+++ lib/project_web/router.ex
@@ -18,6 +18,8 @@ defmodule ProjectWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete],
+      singleton: true
   end
 
   # Other scopes may use custom stacks.
